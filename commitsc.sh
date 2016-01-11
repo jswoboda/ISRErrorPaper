0dc67466ba2186542572eac3ba8117fef088d9eb
John Swoboda <swoboj@bu.edu>
2016-01-11 14:28:49 -0500

    Inital commit of latex files

---
+++ commitsc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+cp -f /Users/Bodangles/Documents/Latex/references/litreview.bib BibTeX/litreview.bib
+#git add BibTeX/litreview.bib 
+#git add spacetimeisramb.tex spacetimeisramb.pdf
+#git add Figs/*.png
